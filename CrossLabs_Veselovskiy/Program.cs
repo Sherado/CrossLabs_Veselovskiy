730ac60bf67a3160e7560618d7af859f661fb1d8
Tsubanari <vshepppardmega@gmail.com>
2022-10-18 14:00:15 +0300

    Добавьте файлы проекта.

---
+++ CrossLabs_Veselovskiy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CrossLabs_Veselovskiy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
